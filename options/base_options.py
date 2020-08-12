06decb57b025b0e544e31ea09e45a71f2e6b2f93
csxmli2016 <csxmli@gmail.com>
2020-08-12 09:37:49 +0800

    Update base_options.py

--- options/base_options.py
+++ options/base_options.py
@@ -19,7 +19,7 @@ class BaseOptions():
         parser.add_argument('--output_nc', type=int, default=3, help='# of output image channels')
         parser.add_argument('--ngf', type=int, default=64, help='# of gen filters in first conv layer')
         parser.add_argument('--which_model_netG', type=str, default='encoderVggDecoderResNet', help='selects model to use for netG')
-        parser.add_argument('--gpu_ids', type=str, default='1,3', help='gpu ids: e.g. 0  0,1,2, 0,2. use -1 for CPU')
+        parser.add_argument('--gpu_ids', type=str, default='0', help='gpu ids: e.g. 0  0,1,2, 0,2. use -1 for CPU')
         parser.add_argument('--name', type=str, default='facefh_dictionary', help='name of the experiment. It decides where to store samples and models')
         parser.add_argument('--dataset_mode', type=str, default='aligned', help='chooses how datasets are loaded. [unaligned | aligned | single]')
         parser.add_argument('--model', type=str, default='faceDict', help='chooses which model to use. cycle_gan, pix2pix, test')
